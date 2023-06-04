6fd88d85b15b4b19ff15d91c93711a1895f3b756
Carter Clifford <carterlc@live.ca>
2023-06-03 23:58:41 -0600

    Put my "Other Projects" section in its own template parts because I will most likely use it more than once.

---
+++ template-parts/project-grid.php
@@ -0,0 +1,76 @@
+<section style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/noise.png), 
+        url(<?php echo get_template_directory_uri(); ?>/assets/images/blurry-white-red.svg);"
+        id="other" class="other dark">
+        <div class="section__container">
+            <div class="heading__container">
+                <h2>Other projects</h2>
+                <p class="sub-heading dash light">almost there...</p>
+            </div>
+            <div class="other__projects">
+                <div class="project__item">
+                    <a href="#">
+                        <img src="assets/images/P2INNACLE_LOGO-50.jpg" alt="ProjectImage">
+                        <div class="project__item-overlay">
+                            <div class="overlay-text">Hover me!</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="project__item">
+                    <a href="#">
+                        <img src="assets/images/P2INNACLE_LOGO-50.jpg" alt="ProjectImage">
+                        <div class="project__item-overlay">
+                            <div class="overlay-text">Hover me!</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="project__item">
+                    <a href="#">
+                        <img src="assets/images/P2INNACLE_LOGO-50.jpg" alt="ProjectImage">
+                        <div class="project__item-overlay">
+                            <div class="overlay-text">Hover me!</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="project__item">
+                    <a href="#">
+                        <img src="assets/images/P2INNACLE_LOGO-50.jpg" alt="ProjectImage">
+                        <div class="project__item-overlay">
+                            <div class="overlay-text">Hover me!</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="project__item">
+                    <a href="#">
+                        <img src="assets/images/P2INNACLE_LOGO-50.jpg" alt="ProjectImage">
+                        <div class="project__item-overlay">
+                            <div class="overlay-text">Hover me!</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="project__item">
+                    <a href="#">
+                        <img src="assets/images/P2INNACLE_LOGO-50.jpg" alt="ProjectImage">
+                        <div class="project__item-overlay">
+                            <div class="overlay-text">Hover me!</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="project__item">
+                    <a href="#">
+                        <img src="assets/images/P2INNACLE_LOGO-50.jpg" alt="ProjectImage">
+                        <div class="project__item-overlay">
+                            <div class="overlay-text">Hover me!</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="project__item">
+                    <a href="#">
+                        <img src="assets/images/P2INNACLE_LOGO-50.jpg" alt="ProjectImage">
+                        <div class="project__item-overlay">
+                            <div class="overlay-text">Hover me!</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
